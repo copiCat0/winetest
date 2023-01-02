54032e11a7ec372e0b1d69c11326bfd65f6ec1a5
copiCat0 <lwj2627@naver.com>
2023-01-02 14:41:31 +0900

    구조 완성

--- javascript/src.js
+++ javascript/src.js
@@ -15,6 +15,24 @@ let q = {
   11:{"title":"문제 11번", "type":"JP", "A":"J", "B":"P"},
   12:{"title":"문제 12번", "type":"JP", "A":"J", "B":"P"}
 };
+let result = {
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ENTP":{"wine":"말벡", "explain":"야생마 같은 말백", "img":"./images/champagne.jpg"},
+  "ENTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+  "ISTJ":{"wine":"샴페인", "explain":"톡톡튀는 샴페인에 대한 설명", "img":"./images/champagne.jpg"},
+};
 
 function start(){
   $(".start").hide();
@@ -32,12 +50,17 @@ function next() {
   if (num == 13){
     $(".question").hide();
     $(".result").show();
-    let mbti = ""
+    let mbti = "";
     ($("EI").val()<2) ? mbti = mbti +"I" : mbti = mbti + "E";
     ($("SN").val()<2) ? mbti = mbti +"N" : mbti = mbti + "S";
     ($("TF").val()<2) ? mbti = mbti +"F" : mbti = mbti + "T";
     ($("JP").val()<2) ? mbti = mbti +"P" : mbti = mbti + "J";
-    alert();
+    alert(mbti);
+
+    $("#img").attr("src", result(mbti)[img]);
+    $("#wine").attr(result(mbti)[wine]);
+    $("#explain").attr(result(mbti)[explain]);
+
   }else{
     $(".progress-bar").attr('style', 'width: calc(100/12*'+num+'%)');
     $("#title").html(q[num]["title"]);
