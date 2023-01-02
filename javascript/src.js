d3f6ff11f9b3f86d9d8d5f28167528d84ef0da7f
copiCat0 <lwj2627@naver.com>
2023-01-02 13:45:54 +0900

    MBTI 결과 코어로직까지

--- javascript/src.js
+++ javascript/src.js
@@ -4,17 +4,17 @@ let num = 1;
 let q = {
   1:{"title":"문제 1번", "type":"EI", "A":"E", "B":"I"},
   2:{"title":"문제 2번", "type":"EI", "A":"E", "B":"I"},
-  3:{"title":"문제 3번", "type":"EI", "A":"E", "B":"I"}
-  5:{"title":"문제 4번", "type":"EI", "A":"S", "B":"N"},
-  4:{"title":"문제 5번", "type":"EI", "A":"S", "B":"N"},
-  6:{"title":"문제 6번", "type":"EI", "A":"S", "B":"N"}
-  7:{"title":"문제 7번", "type":"EI", "A":"T", "B":"F"},
-  9:{"title":"문제 8번", "type":"EI", "A":"T", "B":"F"}
-  8:{"title":"문제 9번", "type":"EI", "A":"T", "B":"F"},
-  10:{"title":"문제 10번", "type":"EI", "A":"J", "B":"P"},
-  11:{"title":"문제 11번", "type":"EI", "A":"J", "B":"P"},
-  12:{"title":"문제 12번", "type":"EI", "A":"J", "B":"P"}
-} 
+  3:{"title":"문제 3번", "type":"EI", "A":"E", "B":"I"},
+  4:{"title":"문제 4번", "type":"SN", "A":"S", "B":"N"},
+  5:{"title":"문제 5번", "type":"SN", "A":"S", "B":"N"},
+  6:{"title":"문제 6번", "type":"SN", "A":"S", "B":"N"},
+  7:{"title":"문제 7번", "type":"TF", "A":"T", "B":"F"},
+  9:{"title":"문제 8번", "type":"TF", "A":"T", "B":"F"},
+  8:{"title":"문제 9번", "type":"TF", "A":"T", "B":"F"},
+  10:{"title":"문제 10번", "type":"JP", "A":"J", "B":"P"},
+  11:{"title":"문제 11번", "type":"JP", "A":"J", "B":"P"},
+  12:{"title":"문제 12번", "type":"JP", "A":"J", "B":"P"}
+};
 
 function start(){
   $(".start").hide();
@@ -32,11 +32,20 @@ function next() {
   if (num == 13){
     $(".question").hide();
     $(".result").show();
-  }
+    let mbti = ""
+    ($("EI").val()<2) ? mbti = mbti +"I" : mbti = mbti + "E";
+    ($("SN").val()<2) ? mbti = mbti +"N" : mbti = mbti + "S";
+    ($("TF").val()<2) ? mbti = mbti +"F" : mbti = mbti + "T";
+    ($("JP").val()<2) ? mbti = mbti +"P" : mbti = mbti + "J";
+    alert();
+  }else{
     $(".progress-bar").attr('style', 'width: calc(100/12*'+num+'%)');
     $("#title").html(q[num]["title"]);
     $("#type").val(q[num]["type"]);
     $("#A").html(q[num]["A"]);
     $("#B").html(q[num]["B"]);
     num++;
+  
+  }
+    
 }; 
